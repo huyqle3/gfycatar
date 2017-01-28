9b86002406bb7001b45779fdfe22822ebe638b50
huyle333 <huyle333@bu.edu>
2017-01-28 17:36:35 -0500

    Added sample mp4 to play on RawImage.

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)
+        {
+            movie.Pause();
+        } else if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)
+        {
+            movie.Play();
+        }
+	}
+}
